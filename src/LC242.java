494d0a7ac5c2b03a8ccacab3a9ff0448adaeaa21
harshdwivedics25-dotcom <harsh885vardhan@gmail.com>
2026-04-10 21:48:57 +0530

    over

---
+++ src/LC242.java
@@ -0,0 +1,16 @@
+class LC242 {
+    public boolean isAnagram(String s, String t) {
+        int[] arr1 = new int[1000];
+        int[] arr2 = new int[1000];
+        for(int i = 0;i < s.length();i++) {
+            arr1[s.charAt(i)]++;
+        }
+        for(int i = 0;i < t.length();i++) {
+            arr2[t.charAt(i)]++;
+        }
+        for(int i = 0;i < arr1.length;i++) {
+            if(arr1[i] != arr2[i]) return false;
+        }
+        return true;
+    }
+}
